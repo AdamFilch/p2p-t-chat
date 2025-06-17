5f957ec9575710229288b073ba849a1146e157fe
Adam Filchoir <filchoiradam@gmail.com>
2025-06-17 17:38:53 +0800

    Chore(chatroom): init chat room area and button for submission

--- src/components/chatroom/chatbox.go
+++ src/components/chatroom/chatbox.go
@@ -1,10 +1,40 @@
 package components_chatroom
 
-import "github.com/rivo/tview"
+import (
+	"github.com/rivo/tview"
+)
 
-func ChatArea() *tview.Box {
-	
-	box := tview.NewBox().SetBorder(true).SetTitle("Chat Area")
+func ChatArea() *tview.TextArea {
 
+	box :=  tview.NewTextArea().SetPlaceholder("Enter your text here...")
+
+	box.SetBorder(true).SetTitle("Chat Area")
 	return box
 }
+
+func SubmitText() *tview.Button {
+	box := tview.NewButton("Submit").SetSelectedFunc(func() {
+	})
+	box.SetBorder(true)
+	return box
+}
+
+// func SubmitText(app ) tview.Primitive {
+// 	text := tview.NewTextView().
+// 		SetText("Click me\nto send your message").
+// 		SetTextAlign(tview.AlignCenter).
+// 		SetDynamicColors(true).
+// 		SetRegions(true).
+// 		SetWordWrap(true).
+// 		SetBorder(true)
+
+// 	text.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+// 		if event.Key() == tcell.KeyEnter {
+// 			// Perform action here
+// 			app.Stop()
+// 		}
+// 		return event
+// 	})
+
+// 	return text
+// }
--- src/pages/chatroom/ChatroomUI.go
+++ src/pages/chatroom/ChatroomUI.go
@@ -1,18 +1,30 @@
 package TUI
 
 import (
+	"fmt"
 	components_chatroom "main/src/components/chatroom"
 
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func Chatroom() *tview.Flex {
+
+
+	input := components_chatroom.ChatArea()
+	input.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		fmt.Println("Key pressed in input area")
+		return event
+	})
+
 	box := tview.NewFlex().SetDirection(tview.FlexColumn).
 		AddItem(components_chatroom.UserList(), 0, 1, false).
 		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
-			AddItem(components_chatroom.ChatHistory(), 0, 1, false).
-			AddItem(components_chatroom.ChatArea(), 0, 1, false),
-			0, 5, false)
+			AddItem(components_chatroom.ChatHistory(), 0, 2, false).
+			AddItem(tview.NewFlex().SetDirection(tview.FlexColumn).
+				AddItem(components_chatroom.ChatArea(), 0, 6, true).
+				AddItem(components_chatroom.SubmitText(), 0, 1, false), 0, 1, true),
+			0, 5, true)
 
 	return box
 }
